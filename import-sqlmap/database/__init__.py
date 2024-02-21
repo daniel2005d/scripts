0035b93178a9f34116ffde0976cbc96b02e2701a
Cyb3rb0b <daniel@daniel.com>
2024-02-21 17:25:17 -0500

    Move import-sqlmap project to mapXplore

--- import-sqlmap/database/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-import os
-root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(root_path, 'database'))
--- import-sqlmap/database/connection.py
+++
@@ -1,9 +0,0 @@
-class Connection:
-    def __init__(self, **kwargs) -> None:
-        for key, item in kwargs.items():
-            setattr(self, key, item)
-    
-
-    server:str=""
-    username:str=""
-    password:str=""
