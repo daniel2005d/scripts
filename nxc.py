0d05c0f76f3a1a4a8705a10d069e0af44509a4cd
Your Name <you@example.com>
2025-05-02 11:06:30 -0400

    fix nxc script

--- nxc.py
+++ nxc.py
@@ -6,8 +6,13 @@ def main():
     if not os.path.isdir("nxc"):
         os.makedirs("nxc")
 
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         print("You need set target")
+        print("Usage:")
+        print(f"{sys.argv[0]} 127.0.0.1")
     else:
-        file_name = f'nxc/{sys.argv[2].replace("/","_")}.log'
+        file_name = f'nxc/{sys.argv[1].replace("/","_")}.log'
         subprocess.run(["nxc"]+sys.argv[1:]+["--log", file_name])
+
+if __name__ == '__main__':
+    main()
